ff5dcc7a1545bbcd8a38059a6e711cb12ab05648
Firider35 <166155163+Firider35@users.noreply.github.com>
2026-03-05 21:33:16 +0100

    Added ton of rocc comments

--- Assets/Resources/Scripts/GameInitializer.cs
+++ Assets/Resources/Scripts/GameInitializer.cs
@@ -21,10 +21,11 @@ namespace Resources.Scripts
 
             // Rocks
             //   IGNEOUS
+            const string igneousBase = "Materials/Rocks/Igneous/";
             //     INTRUSIVE
             //       GRANITE
             const double graniteDensity = 2750.0;
-            const string graniteBase    = "Materials/Rocks/Igneous/Intrusive/Granite/";
+            const string graniteBase    = igneousBase + "Granite/";
             registry.Register("granite_black", new Material(graniteDensity, graniteBase + "granite_black"));
             registry.Register("granite_blue",  new Material(graniteDensity, graniteBase + "granite_blue"));
             registry.Register("granite_gray",  new Material(graniteDensity, graniteBase + "granite_gray"));
@@ -33,17 +34,145 @@ namespace Resources.Scripts
             registry.Register("granite_red",   new Material(graniteDensity, graniteBase + "granite_red"));
             registry.Register("granite_white", new Material(graniteDensity, graniteBase + "granite_white"));
             
+            //       GABBRO
+            
+            //       DIORITE
+            
+            //       PERIDOTITE
+            
+            //       PEGMATITE
+            
+            //       TONALITE
+            
+            //       GRANODIORITE
+            
+            //       ANORTHOSITE
+            
+            //       SYENITE
+            
+            //       NORITE
+            
+            
+            //     EXTRUSIVE
+            //       BASALT
+            
+            //       ANDESITE
+            
+            //       PHONOLITE
+            
+            //       MELAPHYRE
+            
+            //       RHYOLITE
+            
+            //       DACITE
+            
+            //       TRACHYTE
+            
+            //       LATITE
+            
+            //       KOMATIITE
+            
+            
+            //     VOLCANIC GLASS
+            //       OBSIDIAN
+            
+            //       PUMICE
+            
+            //       SCORIA
+            
+            //       TUFF
+            
+            
             //   SEDIMENTARY
+            const string sedimentaryBase = "Materials/Rocks/Sedimentary/";
             //     CLASTIC
             //       CONGLOMERATE
-            registry.Register("conglomerate", new Material(0f, "Materials/Rocks/Sedimentary/Clastic/conglomerate"));
-            registry.Register("breccia", new Material(0f, "Materials/Rocks/Sedimentary/Clastic/breccia"));
-            registry.Register("sand", new Material(0f, "Materials/Rocks/Sedimentary/Clastic/sand"));
-            registry.Register("sandstone", new Material(0f, "Materials/Rocks/Sedimentary/Clastic/sandstone"));
-            registry.Register("gravel", new Material(0f, "Materials/Rocks/Sedimentary/Clastic/gravel"));
+            registry.Register("conglomerate", new Material(0f, sedimentaryBase + "Clastic/conglomerate"));
+            //       BRECCIA
+            registry.Register("breccia", new Material(0f, sedimentaryBase + "Clastic/breccia"));
+            //       SAND
+            registry.Register("sand", new Material(0f, sedimentaryBase + "Clastic/sand"));
+            //       SANDSTONE
+            registry.Register("sandstone", new Material(0f, sedimentaryBase + "Clastic/sandstone"));
+            //       GRAVEL
+            registry.Register("gravel", new Material(0f, sedimentaryBase + "Clastic/gravel"));
+            //       DIRT
+            registry.Register("dirt", new Material(0f, sedimentaryBase + "Clastic/dirt"));
+            //       SLATE
+            const string shaleBase =  sedimentaryBase + "Clastic/Shale/";
+            //       CLAY
+            const string clayBase =  sedimentaryBase + "Clastic/Clay/";
+            //       SILTSTONE
+            
+            //       MUDSTONE
+            
+            //       GRAYWACKE
+            
+            
+            //     BIOGENIC
+            //       LIMESTONE
+            registry.Register("limestone", new Material(0f, sedimentaryBase + "Biogenic/limestone"));
+            //       COAL
+            //         LIGNITE
+            
+            //         SUBBITUMINOUS COAL
+            
+            //         BITUMINOUS COAL
+            
+            //         ANTHRACITE
+            
+            
+            //       COQUINA
+            
+            //       CHALK
+            
+            //       FOSSILIFEROUS LIMESTONE
+            
+            //     CHEMICAL
+            //       TRAVERTINE
+            registry.Register("travertine", new Material(0f, sedimentaryBase + "Chemical/travertine"));
+            //       ROCK SALT
+            
+            //       GYPSUM
+            
+            //       CHERT
+            
+            //       DOLOSTONE
+            
+            
+            //   METAMORPHIC
+            const string metamorphicBase = "Materials/Rocks/Metamorphic/";
+            //     FOLIATED
+            //       SLATE
+            
+            //       PHYLLITE
+            
+            //       SCHIST
+            //         MICA SCHIST
+            
+            //       GNEISS
+            
+            //       MIGMATITE
+            
+            
+            //     NON FOLIATED
+            //       MARBLE
+            
+            //       QUARTZITE
+            
+            //       HORNFELS
+            
+            //       SERPENTINITE
+            
+            //       AMPHIBOLITE
+            
+            
+            
+            
+            //Holy hell the sheer amount of rocks is insane. Just wait for the minerals too, and it will be even worse
             // Soils have been here.
             // Jokes on you. Žádné soily tady nebudou protože žádné soily tady nikdy nebudou :D - Siryakari
-            registry.Register("dirt", new Material(0f, "Materials/Soil/dirt"));
+            
             registry.Register("grass", new Material(0f, "Materials/Soil/grass"));
         }
     }
